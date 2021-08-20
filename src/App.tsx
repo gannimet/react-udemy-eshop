cafaa75dd1be51fe636f6dd0b024f41590c000f8
Richard Wotzlaw <r.wotzlaw@gmail.com>
2021-08-20 11:24:37 +0200

    end of lesson

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,20 @@ import HomePage from './containers/HomePage';
 import AllProductsPage from './containers/AllProductsPage';
 import CheckoutPage from './containers/CheckoutPage';
 import { ROUTE } from './constants/route';
+import HeaderNavigation from './components/HeaderNavigation';
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact component={AllProductsPage} path={ROUTE.ALL_PRODUCTS} />
-        <Route exact component={CheckoutPage} path={ROUTE.CHECKOUT} />
-        <Route exact component={HomePage} path={ROUTE.HOME} />
-        <Redirect to={ROUTE.HOME} />
-      </Switch>
+      <div className="app-container">
+        <HeaderNavigation />
+        <Switch>
+          <Route exact component={AllProductsPage} path={ROUTE.ALL_PRODUCTS} />
+          <Route exact component={CheckoutPage} path={ROUTE.CHECKOUT} />
+          <Route exact component={HomePage} path={ROUTE.HOME} />
+          <Redirect to={ROUTE.HOME} />
+        </Switch>
+      </div>
     </BrowserRouter>
   );
 }
